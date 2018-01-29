7c18922a812093a9976ca417310be4353c1f7831
panda109 <panda109@DESKTOP-RQS9T01>
2018-01-29 17:00:49 +0800

    modify modules

--- app/models.py
+++ app/models.py
@@ -110,3 +110,101 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+class Product(db.Model):
+    """An Products type.
+
+    """
+    __tablename__ = 'products'
+    def __init__(self,
+                 id,
+                 product_type,
+                 common_name,
+                 price, imgurl,
+                 flesh_color,
+                 rind_color,
+                 seedless):
+        self.id = id
+        self.product_type = product_type
+        self.common_name = common_name
+        self.price = price
+        self.imgurl = imgurl
+        self.flesh_color = flesh_color
+        self.rind_color = rind_color
+        self.seedless = bool(seedless)
+
+    def price_str(self):
+        """Return price formatted as string $x.xx"""
+
+        return "$%.2f" % self.price
+
+    def __repr__(self):
+        """Convenience method to show information about melon in console."""
+
+        return "<Item: %s, %s, %s>" % (
+            self.id, self.common_name, self.price_str())
+
+    @classmethod
+    def get_all(cls, max=30):
+        """Return list of melons.
+
+        Query the database for the first [max] melons, returning each as a
+        Melon object
+        """
+
+        cursor = db_connect()
+        QUERY = """
+                  SELECT id,
+                         product_type,
+                         common_name,
+                         price,
+                         imgurl,
+                         flesh_color,
+                         rind_color,
+                         seedless
+                   FROM products
+                   WHERE imgurl <> ''
+                   LIMIT ?;
+               """
+
+        cursor.execute(QUERY, (max,))
+        product_rows = cursor.fetchall()
+
+        # list comprehension to build a list of Melon objects by going through
+        # the database records and making a melon for each row. This is done
+        # by unpacking in the for-loop.
+
+        product = [Product(*row) for row in product_rows]
+
+        print product
+
+        return product
+
+    @classmethod
+    def get_by_id(cls, id):
+        """Query for a specific melon in the database by the primary key"""
+
+        cursor = db_connect()
+        QUERY = """
+                  SELECT id,
+                         product_type,
+                         common_name,
+                         price,
+                         imgurl,
+                         flesh_color,
+                         rind_color,
+                         seedless
+                   FROM products
+                   WHERE id = ?;
+               """
+
+        cursor.execute(QUERY, (id,))
+
+        row = cursor.fetchone()
+
+        if not row:
+            return None
+
+        product = Product(*row)
+
+        return product
