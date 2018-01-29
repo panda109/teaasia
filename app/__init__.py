7d1097fd617922454a4523743dc35a33aaa9ff84
panda109 <panda109@DESKTOP-RQS9T01>
2018-01-29 20:33:01 +0800

    update

--- app/__init__.py
+++ app/__init__.py
@@ -34,5 +34,6 @@ def create_app(config_name):
     app.register_blueprint(main_blueprint)
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
-
+    from .products import products as products_blueprint
+    app.register_blueprint(products_blueprint)
     return app
