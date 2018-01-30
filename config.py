076a8b8091f243537f9f36b522d90426d61d5560
panda109 <panda109@DESKTOP-RQS9T01>
2018-01-30 17:05:14 +0800

    merger email and product

--- config.py
+++ config.py
@@ -7,24 +7,21 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    #yhkflwbqspbfzohk
-    SECRET_KEY = 'hard to guess string'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_USE_TLS = False
-    MAIL_PORT = 465
-    MAIL_USE_SSL = True
-    #MAIL_SERVER = 'smtp.googlemail.com'
-    #MAIL_PORT = 587
-    #MAIL_USE_TLS = True
-    #MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    #MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    MAIL_USERNAME = 'yr6703@gmail.com'
-    MAIl_PASSWORD = ''
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'teaasia5812@gmail.com'
+    MAIL_PASSWORD = ''
+    #MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    #MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    #print MAIL_USERNAME
+    #print MAIL_PASSWORD
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'yr6703@gmail.com'
-    FLASKY_ADMIN = 'FLASKY_ADMIN'
+    FLASKY_MAIL_SENDER = 'TeaAsia Admin <tesaasia5812@gmail.com>'
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
 
     @staticmethod
     def init_app(app):
