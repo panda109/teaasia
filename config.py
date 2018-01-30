ad42aba0fa0c6d59bb02ca1d4dcd6e3fd53402d4
panda109 <panda109@DESKTOP-RQS9T01>
2018-01-30 16:19:46 +0800

    QQ

--- config.py
+++ config.py
@@ -11,19 +11,19 @@ class Config(object):
     SECRET_KEY = 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    #MAIL_SERVER = 'smtp.gmail.com'
-    #MAIL_USE_TLS = False
-    #MAIL_PORT = 465
-    #MAIL_USE_SSL = True
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_USE_TLS = False
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+    #MAIL_SERVER = 'smtp.googlemail.com'
+    #MAIL_PORT = 587
+    #MAIL_USE_TLS = True
     #MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     #MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     MAIL_USERNAME = 'yr6703@gmail.com'
-    Mail_PASSWORD = 'Panda109'
+    MAIl_PASSWORD = ''
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <yr6703@gmail.com>'
+    FLASKY_MAIL_SENDER = 'yr6703@gmail.com'
     FLASKY_ADMIN = 'FLASKY_ADMIN'
 
     @staticmethod
