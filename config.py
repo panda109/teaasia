7838ce34ed7829558d825fb9e50b2cb0a5686ad9
panda109 <panda109@DESKTOP-RQS9T01>
2018-02-07 21:42:07 +0800

    order.html update

--- config.py
+++ config.py
@@ -5,14 +5,14 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or '!QIOD*Lioisfhishiwiwe98ew9233'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = 'teaasia5812@gmail.com'
-    MAIL_PASSWORD = ''
+    MAIL_PASSWORD = 'panda109'
     #MAIL_USERNAME = os.getenv('MAIL_USERNAME')
     #MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[TeaAsia]'
