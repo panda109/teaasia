b6d608fc619b604c8f7222e51791b1348ded0514
panda109 <panda109@DESKTOP-RQS9T01>
2018-02-15 17:19:06 +0800

    modigy config.py

--- config.py
+++ config.py
@@ -7,17 +7,21 @@ UPLOADPATH = os.getcwd() + '\\static\\_uploads\\images\\'
 print UPLOADPATH
 IMAGEPATH = os.getcwd() + '\\static\\product\\images\\'
 print IMAGEPATH
+servername = 'http://54.95.82.93:5000'
+print servername
+mailpassword='grace2402'
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or '!QIOD*Lioisfhishiwiwe98ew9233'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     #un mark SERVER_NAME {{ url_for('auth.confirm', token=token, _external=True) }}
-    #SERVER_NAME = 'http://54.95.82.93:5000'
+    SERVER_NAME = servername
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = 'teaasia5812@gmail.com'
-    MAIL_PASSWORD = ''
+    MAIL_PASSWORD = mailpassword
     # MAIL_USERNAME = os.getenv('MAIL_USERNAME')
     # MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[TeaAsia]'
@@ -28,7 +32,6 @@ class Config(object):
     @staticmethod
     def init_app(app):
         pass
-
 
 class DevelopmentConfig(Config):
     DEBUG = True
