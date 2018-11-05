ff9cd1a98445104e602370ba71b2c91f7c37fdc7
panda109 <panda109@panda109-HP-ProBook-6470b>
2018-11-05 11:21:41 +0800

    add video

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
 # config.py
 import os
-#basedir = os.path.abspath(os.path.dirname(__file__))
+basedir = os.path.abspath(os.path.dirname(__file__))
 #UPLOADPATH = os.getcwd() + '\\static\\_upload\\images\\'
 #P_IMAGEPATH = os.getcwd() + '\\static\\product\\images\\'
 #S_IMAGEPATH = os.getcwd() + '\\static\\story\\images\\'
 
-#UPLOADPATH = os.getcwd() + '/static/_upload/images/'
-#P_IMAGEPATH = os.getcwd() + '/static/product/images/'
-#S_IMAGEPATH = os.getcwd() + '/static/story/images/'
+UPLOADPATH = os.getcwd() + '/static/_upload/images/'
+P_IMAGEPATH = os.getcwd() + '/static/product/images/'
+S_IMAGEPATH = os.getcwd() + '/static/story/images/'
 
 #IPPORT = 'bytaiwan.me'
-#IPPORT = '10.90.7.37:5000'
+IPPORT = '172.24.12.22:5000'
 #IPPORT = '192.168.0.18:5000'
 #IPPORT = '29fa258b.ngrok.io'
-mailpassword=''
+mailpassword='grace2402'
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or '!QIOD*Lioisfhishiwiwe98ew9233'
